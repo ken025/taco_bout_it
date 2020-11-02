641d364f95646f798007d88ed73d63161335ec0f
ken025 <kennethmanuel250@gmail.com>
2020-11-02 18:29:01 -0500

    added restaurants resources

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_31_021153) do
+ActiveRecord::Schema.define(version: 2020_11_02_192721) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
