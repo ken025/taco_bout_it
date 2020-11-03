ff3250502f5ed0e8d72943be1e585606d0748f46
ken025 <kennethmanuel250@gmail.com>
2020-11-02 20:12:12 -0500

    addd image to users table

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_11_02_192721) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "username"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
