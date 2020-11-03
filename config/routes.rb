382deca0c2b98d52171cd4f0fd30fb5bcb260298
ken025 <kennethmanuel250@gmail.com>
2020-11-02 20:13:07 -0500

    changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :restaurants
   devise_for :users, :controllers => {registrations: 'registrations' }
   # omniauth_callbacks: 'users/omniauth_callbacks' 
 
 # Routes for Google authentication
-get 'auth/:provider/callback', to: 'sessions#googleAuth'
+get 'auth/:provider/callback', to: 'sessions#omniauth'
 get 'auth/failure', to: redirect('/')
 
-  root to: "home#index"
+root to: "users#show"
 end
