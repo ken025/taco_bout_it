a98c8f6a495fd255cbf3cf9aebdabfd9472d26a6
ken025 <kennethmanuel250@gmail.com>
2020-11-03 22:59:13 -0500

    added methods

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,35 @@
 class RestaurantsController < ApplicationController
-end
+
+    def index
+        @restaurants = Restaurant.all
+      end
+  
+      def show
+      end
+  
+      def new
+        @restaurant = Restaurant.new
+      end
+  
+      def edit 
+      end 
+
+      def create
+        binding.pry
+        @restaurant = Restaurant.new(restaurant_params)
+        binding.pry
+        if @restaurant.save
+          redirect_to restaurant_path(@restaurant)
+        end
+      end
+
+      private
+
+      def set_restaurant
+        @restaurant = Restaurant.find(params[:id])
+      end
+  
+      def restaurant_params
+        params.require(:restaurant).permit(:name, :location, :review, :stars)
+      end
+  end
