4db0c7a4b9647ed0fba450ea377ebe6462b35205
ken025 <kennethmanuel250@gmail.com>
2020-11-05 15:23:40 -0500

    updated schema

--- db/migrate/20201104035612_add_column_to_reviews.rb
+++ db/migrate/20201104035612_add_column_to_reviews.rb
@@ -1,6 +1,6 @@
 class AddColumnToReviews < ActiveRecord::Migration[6.0]
   def change
-    add_column :reviews, :message, :integer
+    add_column :reviews, :message, :string
     add_column :reviews, :stars, :float
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_035612) do
+ActiveRecord::Schema.define(version: 2020_11_05_154203) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -23,9 +23,11 @@ ActiveRecord::Schema.define(version: 2020_11_04_035612) do
     t.integer "restaurant_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "message"
+    t.string "message"
     t.float "stars"
+    t.integer "user_id"
     t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,4 +49,5 @@ ActiveRecord::Schema.define(version: 2020_11_04_035612) do
   end
 
   add_foreign_key "reviews", "restaurants"
+  add_foreign_key "reviews", "users"
 end
